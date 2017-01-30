4d7737e61adbabcc6e1f0cdfb53b20c85e2e536f
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-30 12:30:03 -0600

    Completed "Hello Worl!" Exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,33 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet (name = "HelloServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if (name.isEmpty()) {
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+
+            out.println("Hello " + name + "!");
+
+        }
+
+
+
 
     }
+
+
 }
